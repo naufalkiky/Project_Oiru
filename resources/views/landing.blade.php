b73828295477532675f219343f44a504cd0bc62c
Rifal <rifalariya13@gmail.com>
2025-01-10 21:08:18 +0700

    Tambah Logo

--- resources/views/landing.blade.php
+++ resources/views/landing.blade.php
@@ -44,7 +44,8 @@
       <a href="index.html" class="logo d-flex align-items-center me-auto">
         <!-- Uncomment the line below if you also wish to use an image logo -->
         <!-- <img src="assets/img/logo.png" alt=""> -->
-        <h1 class="sitename">Scaffold</h1>
+        <img src="{{ asset('Image/LogoOiru.png') }}">
+        {{-- <h1 class="sitename">Scaffold</h1> --}}
       </a>
 
       <nav id="navmenu" class="navmenu">
